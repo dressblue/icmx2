c98463447703c04ce05d2aadf99d5cef00bee527
Richard Gordon <rgordon@itllc.com>
2016-03-22 12:46:24 -0400

    Tues Afternoon

---
+++ icmx2/icmx2.HTMLClient/Screens/StatusSet/ViewStatus.lsml.js
@@ -0,0 +1,10 @@
+﻿/// <reference path="~/GeneratedArtifacts/viewModel.js" />
+
+myapp.ViewStatus.Details_postRender = function (element, contentItem) {
+    // Write code here.
+    var name = contentItem.screen.ContractStatusSet.details.getModel()[':@SummaryProperty'].property.name;
+    contentItem.dataBind("screen.ContractStatusSet." + name, function (value) {
+        contentItem.screen.details.displayName = value;
+    });
+}
+
