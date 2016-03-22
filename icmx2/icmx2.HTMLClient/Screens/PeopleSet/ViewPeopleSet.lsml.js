87be62c7395d62d1de3d2280081b6b87f6b41085
Richard Gordon <rgordon@itllc.com>
2016-03-22 07:44:36 -0400

    Tuesday Morning

---
+++ icmx2/icmx2.HTMLClient/Screens/PeopleSet/ViewPeopleSet.lsml.js
@@ -0,0 +1,10 @@
+﻿/// <reference path="~/GeneratedArtifacts/viewModel.js" />
+
+myapp.ViewPeopleSet.Details_postRender = function (element, contentItem) {
+    // Write code here.
+    var name = contentItem.screen.PeopleSet.details.getModel()[':@SummaryProperty'].property.name;
+    contentItem.dataBind("screen.PeopleSet." + name, function (value) {
+        contentItem.screen.details.displayName = value;
+    });
+}
+
