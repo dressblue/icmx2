ad5089680ed2c7a67bcc51b82bc5bc9ffe68b344
Richard Gordon <rgordon@itllc.com>
2016-03-28 13:00:23 -0400

    Monday morning

---
+++ icmx2/icmx2.HTMLClient/Screens/LaborCategoryAssignmentsSet/ViewLaborCategoryAssignmentsSet.lsml.js
@@ -0,0 +1,10 @@
+﻿/// <reference path="~/GeneratedArtifacts/viewModel.js" />
+
+myapp.ViewLaborCategoryAssignmentsSet.Details_postRender = function (element, contentItem) {
+    // Write code here.
+    var name = contentItem.screen.LaborCategoryAssignmentsSet.details.getModel()[':@SummaryProperty'].property.name;
+    contentItem.dataBind("screen.LaborCategoryAssignmentsSet." + name, function (value) {
+        contentItem.screen.details.displayName = value;
+    });
+}
+
