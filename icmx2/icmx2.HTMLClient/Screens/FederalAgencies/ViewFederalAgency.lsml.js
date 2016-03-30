630829e7abd0c3db60c20c2fec621247adbf47e1
Richard Gordon <rgordon@itllc.com>
2016-03-29 22:08:59 -0400

    Tues Late

---
+++ icmx2/icmx2.HTMLClient/Screens/FederalAgencies/ViewFederalAgency.lsml.js
@@ -0,0 +1,10 @@
+﻿/// <reference path="~/GeneratedArtifacts/viewModel.js" />
+
+myapp.ViewFederalAgency.Details_postRender = function (element, contentItem) {
+    // Write code here.
+    var name = contentItem.screen.FederalAgency.details.getModel()[':@SummaryProperty'].property.name;
+    contentItem.dataBind("screen.FederalAgency." + name, function (value) {
+        contentItem.screen.details.displayName = value;
+    });
+}
+
