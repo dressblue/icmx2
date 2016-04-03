8cab56b3a44d70118a2e8aed09d0546f863a92fe
Richard Gordon <rgordon@itllc.com>
2016-04-02 21:49:25 -0400

    Tues 2

---
+++ icmx2/icmx2.HTMLClient/Screens/LaborDataCollectors/ViewLaborDataCollector.lsml.js
@@ -0,0 +1,10 @@
+﻿/// <reference path="~/GeneratedArtifacts/viewModel.js" />
+
+myapp.ViewLaborDataCollector.Details_postRender = function (element, contentItem) {
+    // Write code here.
+    var name = contentItem.screen.LaborDataCollector.details.getModel()[':@SummaryProperty'].property.name;
+    contentItem.dataBind("screen.LaborDataCollector." + name, function (value) {
+        contentItem.screen.details.displayName = value;
+    });
+}
+
